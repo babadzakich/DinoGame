3df80676e5ee14d6eaa7e91c3f366906b2fc61eb
babadzakich <doctorare@mail.ru>
2024-04-29 13:09:14 +0700

    chtoto sdelal

--- program.asm
+++ program.asm
@@ -2,19 +2,23 @@ asect 0xf3
 lowerBitCounter:
 asect 0xf4
 upperBitCounter:
+asect 0xf5
+delayerBit:
 asect 0x00
 start:
 ldi r0, lowerBitCounter
 ldi r2, upperBitCounter
-ldi r1, 0
+ldi r3, 64
 counter:
 	do
+		ldc r1, r0
 		inc r1
 		st r0, r1
 		tst r1
 	until mi
 	ldi r1, 0
-	ldc r3, r2
+	st r0, r1	
+	ldc r2, r3
 	inc r3
 	st r2, r3
 	br counter
